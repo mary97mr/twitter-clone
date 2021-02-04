ee4d0bae3c010e90ecf8f9a511aad64fef6706dc
Mari <marymendirojas@gmail.com>
2021-02-04 15:20:11 +0100

    fixed delete reply from user tweets array

--- routes/replies.js
+++ routes/replies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true});
 const catchAsync = require("../utils/catchAsync");
 const Tweet = require("../models/tweet");
+const User = require("../models/user");
 const { isLoggedIn, validateTweet, isReplyAuthor } = require("../middleware");
 
 //Multer is a middleware that divides our req.body and req.files => [{info pic1},{info pic2},{}...]
@@ -12,13 +13,20 @@ const upload = multer({ storage }); // Now pics will be save in our storage.
 // ------Create reply route--------//
 router.post("/", isLoggedIn, upload.array("image"), validateTweet, catchAsync(async (req, res, next) => {
     const tweet = await Tweet.findById(req.params.id).populate("replies");
+    const currentUser = await User.findById(req.user._id);
+    // create and save reply
     const replyTweet = new Tweet(req.body.tweet);
     replyTweet.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    replyTweet.author = req.user._id;
+    replyTweet.author = currentUser._id;
     replyTweet.parent = tweet;
     await replyTweet.save();
+    // push reply into replies and save
     await tweet.replies.push(replyTweet);
     await tweet.save();
+    // push replyTweet into user tweets and save
+    await currentUser.tweets.unshift(replyTweet);
+    await currentUser.save();
+    // redirect
     req.flash("success", "Successfully created a new post");
     res.redirect(`/tweets/${tweet._id}`)
 }));
@@ -26,8 +34,13 @@ router.post("/", isLoggedIn, upload.array("image"), validateTweet, catchAsync(as
 // ------Delete reply route ----
 router.delete("/:replyId", isLoggedIn, isReplyAuthor, catchAsync(async (req, res) => {
     const { id, replyId } = req.params;
+    // Remove reply from replies array
     await Tweet.findByIdAndUpdate(id, { $pull: { replies: replyId } });
-    await Tweet.findByIdAndDelete( replyId );
+    // Remove reply from user tweets array
+    await User.findByIdAndUpdate(req.user._id, { $pull: { tweets: replyId } });
+    // Delete tweet
+    await Tweet.findByIdAndDelete(replyId);
+    // redirect
     req.flash("success", "You deleted your tweet");
     res.redirect(`/tweets/${id}`)
 }));
