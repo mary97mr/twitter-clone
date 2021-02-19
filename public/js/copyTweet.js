bad2a3a861a761326547bef8e8572b6004c00a3b
Mari <marymendirojas@gmail.com>
2021-02-19 18:11:22 +0100

    changed url copy function

--- public/js/copyTweet.js
+++ public/js/copyTweet.js
@@ -1,6 +1,6 @@
 const copyTweet = (tweetId) => {
     const temp = document.createElement("input");
-    const tweet_url = "http://localhost:3000/tweets/" + tweetId;
+    const tweet_url = "https://twitter-mary97mr.herokuapp.com/tweets/" + tweetId;
     document.body.appendChild(temp);
     temp.value = tweet_url;
     temp.select();   
