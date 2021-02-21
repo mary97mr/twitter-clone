252c5b1459f768f408c74aec516cacbc0318f4cd
Mari <marymendirojas@gmail.com>
2021-02-21 17:54:51 +0100

    Added forgot password

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
 const User = require("../models/user");
+const async = require("async");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 const { isLoggedIn, isLoggedOut, isUserProfile, searching } = require("../middleware");
 
 const multer = require("multer");
@@ -139,6 +142,130 @@ router.get("/follow/:userId", isLoggedIn, catchAsync(async (req, res) => {
         res.redirect("back");
     }
 }));
+
+// Display forgot password form
+router.get("/forgot", (req, res) => {
+    res.render("users/forgot");
+});
+
+// Forgot password logic
+router.post("/forgot", function(req, res, next) {
+    async.waterfall([
+        function(done) {
+            crypto.randomBytes(20, (err, buf) => {
+                let token = buf.toString("hex");
+                done(err, token);
+            });
+        },
+        function(token, done) {
+            User.findOne({ email: req.body.email }, function(err, user) {
+                if (!user) {
+                    req.flash("error", "No account with that email address exists.");
+                    return res.redirect("/forgot");
+                }
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+                user.save(function (err) {
+                    done(err, token, user);
+                });
+            });
+        },
+        function(token, user, done) {
+            const smtpTransport = nodemailer.createTransport({
+                service: "Gmail",
+                auth: {
+                    user: "yelpcodinginfo@gmail.com",
+                    pass: process.env.GMAILPW
+                }
+            });
+            const mailOptions = {
+                to: user.email,
+                from: "yelpcodinginfo@gmail.com",
+                subject: "Twitter Clone Password Reset",
+                text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n 
+                Please click on the following link, or paste this into your browser to complete the process:\n\n
+                http://${req.headers.host}/reset/${token} \n\n 
+                If you did not request this, please ignore this email and your password will remain unchanged.\n`
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+                console.log("mail sent");
+                req.flash("success", `An email has been sent to ${user.email} with further instructions.`);
+                done(err, "done");
+            });
+        }
+    ], function(err) {
+        if(err) return next(err);
+        res.redirect("/forgot");
+    });
+});
+
+// Display Reset password form
+router.get("/reset/:token", async (req, res) => {
+    const { token } = req.params;
+    const user = await User.findOne({
+        resetPasswordToken: req.params.token,
+        resetPasswordExpires: { $gt: Date.now() }
+    });
+    if(!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/forgot");
+    }
+    res.render("users/reset", { token });
+});
+
+
+router.post("/reset/:token", (req, res) => {
+    async.waterfall([
+        function(done) {
+            User.findOne({
+                resetPasswordToken: req.params.token,
+                resetPasswordExpires: { $gt: Date.now() }
+            }, function (err, user) {
+                if(!user) {
+                    req.flash("error", "Password reset token is invalid or has expired.");
+                    return res.redirect("back");
+                }
+                if(req.body.password === req.body.confirm) {
+                    user.setPassword(req.body.password, err => {
+                        user.resetPasswordToken = undefined;
+                        user.resetPasswordExpires = undefined;
+    
+                        user.save(function(err) {
+                            req.logIn(user,  err => {
+                                done(err, user);
+                            });
+                        });
+                    });
+                } else {
+                    req.flash("error", "Passwords do not match");
+                    return res.redirect("back");
+                }
+            });
+        },
+        function(user, done) {
+            const smtpTransport = nodemailer.createTransport({
+                service: "Gmail",
+                auth: {
+                    user: "yelpcodinginfo@gmail.com",
+                    pass: process.env.GMAILPW
+                }
+            });
+            const mailOptions = {
+                to: user.email,
+                from: "yelpcodinginfo@gmail.com",
+                subject: "Your password has been changed.",
+                text: `Hello, \n\n
+                This is a comfirmation that the password for your account ${user.email} has just been changed.`
+            };
+            smtpTransport.sendMail(mailOptions, (err) => {
+                req.flash("success", "Success! Your password has been changed.");
+                done(err);
+            });
+        }
+    ], function(err) {
+        res.redirect("/twitter/home");
+    });
+});
 
 module.exports = router;
 
